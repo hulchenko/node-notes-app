42101b5275a27bdb799f04841d24ca324ef2e1d0
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-30 23:21:22 -0600

    add comments

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+//npm modules import
 const validator = require('validator'); // = ./node_modules/validator
 const chalk = require('chalk');
+//node import
 const getNotes = require('./notes.js');
+// ---
 const msg = getNotes();
 console.log(msg);
 console.log(validator.isURL('google.ca'));
