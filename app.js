133adfd956fb545f864825631b943dfcf9d56493
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-31 01:01:45 -0600

    add functionality to add command

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const validator = require('validator'); // = ./node_modules/validator
 const chalk = require('chalk');
 const yargs = require('yargs');
 // node import
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 // ---
 
 // Customize yargs version
@@ -26,8 +26,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title: ', argv.title);
-    console.log('Body: ', argv.body);
+    notes.addNote(argv.title, argv.body); //in notes.js
   },
 });
 
--- notes.js
+++ notes.js
@@ -1,5 +1,47 @@
+const fs = require('fs');
+
 const getNotes = function () {
   return 'Test notes..';
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  // command would load array of the existing notes(different notes var than in app.js)
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      // values come from arguments in addNote function
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log('New note was created!');
+  } else {
+    console.log('Note name already exists');
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = function () {
+  // adds value to the existing array, does not overwrite it
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON); // returns final object-parsed value from JSON file
+  } catch (error) {
+    // if file doesn't exist -> return empty array
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
