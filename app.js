78b398b746edf752f40eea2e9e9197c1318f7289
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-30 23:54:42 -0600

    add commands

--- app.js
+++ app.js
@@ -1,10 +1,62 @@
-//npm modules import
+// npm modules import
 const validator = require('validator'); // = ./node_modules/validator
 const chalk = require('chalk');
-//node import
+const yargs = require('yargs');
+// node import
 const getNotes = require('./notes.js');
 // ---
-const msg = getNotes();
-console.log(msg);
-console.log(validator.isURL('google.ca'));
-console.log(chalk.red('Hello!'));
+
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note Title',
+      demandOption: true, //command arguments required
+      type: 'string',
+    },
+    body: {
+      describe: 'This is a body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Title: ', argv.title);
+    console.log('Body: ', argv.body);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Note removed');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading a note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List a note',
+  handler: function () {
+    console.log('Note listed');
+  },
+});
+
+// add, remove, read, list
+yargs.parse(); //displays output in the command line
--- cfg.md
+++ cfg.md
@@ -1,5 +1,7 @@
 <!-- to recreate npm modules, simply run npm install(it will look into package-lock.json and package.json dependencies/versions and install them automatically) -->
+<!-- Node: global / process = JS: window / document -->
 
 npm i validator (strings validator)
 npm i chalk (terminal output logs styler)
-npm install -g nodemon (node auto refresh/run)(installed globally)
+npm i -g nodemon (node auto refresh/run)(installed globally)
+npm i yargs (parsing command arguments)
